0aeceda9855815cd2f82a07a4aa9bc3e33406590
robim16 <carteagajimenez@gmail.com>
2025-12-03 14:48:24 -0500

    cambiios typeorm

--- apps/tickets-service/typeorm.config.ts
+++ apps/tickets-service/typeorm.config.ts
@@ -8,7 +8,7 @@ const dataSource = new DataSource({
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 5432,
   username: process.env.DB_USERNAME || 'postgres',
-  password: 'Pwsputnik16%',
+  password: process.env.DB_PASSWORD || 'password',
   database: process.env.DB_NAME || 'ticketsdb',
 
 
