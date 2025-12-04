c5bc19bef9db92c18528167b6e7e5ffbaa2dd0db
robim16 <carteagajimenez@gmail.com>
2025-12-04 16:16:30 -0500

    conexion a rqm ticket

--- apps/tickets-service/src/main.ts
+++ apps/tickets-service/src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { TicketsServiceModule } from './tickets-service.module';
-import * as dotenv from 'dotenv';
 
 async function bootstrap() {
-  dotenv.config();
-  const app = await NestFactory.create(TicketsServiceModule);
-  await app.listen(process.env.port ?? 4001);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    TicketsServiceModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://localhost:5672'],
+        queue: 'tickets_queue',
+        queueOptions: { durable: true },
+      },
+    },
+  );
+
+  await app.listen();
+  console.log('Tickets-service microservice is running...');
 }
 bootstrap();
